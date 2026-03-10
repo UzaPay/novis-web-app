2f679530bc17ebc913b3c863d958743ddb50514d
Martin <technology@jahazii.io>
2026-03-10 21:46:19 +0300

    feat: enhance Vite configuration with Brotli compression and improved build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+   base: "/",
+   build: {
+    outDir: "dist",
+    target: "es2019",
+    sourcemap: false,
+    minify: "esbuild",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"]
+        }
+      }
+    }
+  },
+  plugins: [
+    react(),
+    compression({
+      algorithm: "brotliCompress",
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 3001,
     open: true,
   },
-})
+});
